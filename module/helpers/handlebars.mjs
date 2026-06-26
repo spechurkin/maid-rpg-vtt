6bdfb394d2a282d9cc0ac4895629cd5c7695c360
Svyatoslav Pechurkin <51242338+spechurkin@users.noreply.github.com>
2026-06-26 21:49:09 +0300

    Use rich text editors on character sheets

--- module/helpers/handlebars.mjs
+++ module/helpers/handlebars.mjs
@@ -3,4 +3,5 @@ export function registerHandlebarsHelpers() {
     Handlebars.registerHelper("maidChecked", (value) => value ? "checked" : "");
     Handlebars.registerHelper("eq", (left, right) => left === right);
     Handlebars.registerHelper("multiply", (left, right) => Number(left || 0) * Number(right || 0));
+    Handlebars.registerHelper("concat", (...parts) => parts.slice(0, -1).join(""));
 }
--- module/sheets/actor-sheet.mjs
+++ module/sheets/actor-sheet.mjs
@@ -13,6 +13,26 @@ export class MaidActorSheet extends foundry.appv1.sheets.ActorSheet {
         const context = await super.getData(options);
         context.config = CONFIG.MAID_RPG;
         context.system = this.actor.system;
+
+        const enrichOptions = {async: true, secrets: this.actor.isOwner};
+        const enrichText = (value) => TextEditor.enrichHTML(value ?? "", enrichOptions);
+        const enrichTextArray = (values = []) => Promise.all(values.map((value) => enrichText(value)));
+
+        context.enrichedRootDescription = await enrichText(this.actor.system.rootDescription);
+        context.enrichedStressExplosionDescription = await enrichText(this.actor.system.stressExplosionDescription);
+        context.enrichedMaidPowerDescription = await enrichText(this.actor.system.maidPowerDescription);
+        context.enrichedSpecialQualityDescriptions = await enrichTextArray(this.actor.system.specialQualityDescriptions);
+        context.enrichedStressNotes = await enrichText(this.actor.system.stressNotes);
+        context.enrichedMaidWeapons = await enrichText(this.actor.system.maidWeapons);
+        context.enrichedFavorNotes = await enrichText(this.actor.system.favorNotes);
+        context.enrichedTrauma = await enrichText(this.actor.system.trauma);
+        context.enrichedSeduction = await enrichText(this.actor.system.seduction);
+        context.enrichedDescription = await enrichText(this.actor.system.description);
+        context.enrichedNotes = await enrichText(this.actor.system.notes);
+        context.enrichedButlerPowerDescriptions = await enrichTextArray(this.actor.system.butlerPowerDescriptions);
+        context.enrichedPowerSourceDescriptions = await enrichTextArray(this.actor.system.powerSourceDescriptions);
+        context.enrichedMansion = await enrichText(this.actor.system.mansion);
+        context.enrichedEncounterTable = await enrichText(this.actor.system.encounterTable);
         return context;
     }
 
